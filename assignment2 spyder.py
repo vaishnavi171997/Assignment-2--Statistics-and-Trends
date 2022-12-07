f5c4feacdf8e4ab10b9cbc921fe0d086c01f3661
vaishnavi171997 <116464260+vaishnavi171997@users.noreply.github.com>
2022-12-07 14:00:53 +0000

    Defining Function to produce two data frames
    Defining function to produce two data frames, one with countries as columns and one with years as columns

--- assignment2 spyder.py
+++ assignment2 spyder.py
@@ -10,7 +10,23 @@ import pandas as pd
 import numpy as np
 #Importing matplotlib package for data plotting and visualization
 import matplotlib.pyplot as plt
-#Importing scipy functions to calculate skewness and kurtoisis
+#Importing scipy functions to calculate skewnwss and kurtoisis
 import scipy.stats as st
 
 
+#defining function to produce two dataframes,one with countries as columns and one with years as coulmns
+def readcsv(input_file,countries):
+    data = pd.read_csv(input_file)
+    #replacing the null values with zeroes using fillna() method
+    dropping_values = data.fillna(0) 
+    test = dropping_values[dropping_values['Country Name'].isin(countries)]
+    df_countries = pd.DataFrame(test)
+    print(df_countries)
+    #transposing data to derive dataframe with years as columns
+    transpose=pd.DataFrame.transpose(test)
+    header = transpose.iloc[0].values.tolist()
+    transpose.columns = header
+    transpose = transpose.iloc[0:]
+    df_years = transpose
+    print(transpose)
+    return df_countries,df_years
