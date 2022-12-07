d50adc11365c4431bae2ad5d1766c861133f92a3
vaishnavi171997 <116464260+vaishnavi171997@users.noreply.github.com>
2022-12-07 19:34:13 +0000

    Update assignment2 spyder.py

--- assignment2 spyder.py
+++ assignment2 spyder.py
@@ -35,9 +35,11 @@ def readcsv(input_file,countries):
 #calling the function to produce two dataframes by choosing few countries
 df_co,df_yr=readcsv('API_19_DS2_en_csv_v2_4700503-Copy.csv',['Belgium','Bulgaria','Colombia','Finland','United Kingdom'])
 
-#Deriving statistical properties
-#using pandas describe function to derive statistical properties
-print('Describe\n',df_yr.iloc[4:,10:20].describe())
+#Calculating and comparing statistical properties for five different countries comparing different indicators
+
+#using pandas describe function to calculate statistical properties for five countries
+print("\nCalculating describe of Methane emissions (% change from 1990) for five different countries")
+print('\nDescribe\n',df_yr.iloc[4:,[31,107,183,259,335]].describe())
 
 #Calculating mean using numpy
 print('Mean\n', np.mean(df_yr.iloc[4:,10:20]))
