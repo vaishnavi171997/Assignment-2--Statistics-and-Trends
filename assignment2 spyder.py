f7a97e206fcea3c5fb282f268de7592950ef40c6
vaishnavi171997 <116464260+vaishnavi171997@users.noreply.github.com>
2022-12-07 13:46:19 +0000

    Importing python libraries
    Importing Pandas, NumPy, Matplotlib and SciPy libraries

---
+++ assignment2 spyder.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 27 10:53:24 2022
+
+@author: HP
+"""
+#Importing pandas package to read the file,for deriving pandas features and stats properties
+import pandas as pd
+#Importing numpy to read statistical properties
+import numpy as np
+#Importing matplotlib package for data plotting and visualization
+import matplotlib.pyplot as plt
+#Importing scipy functions to calculate skewnwss and kurtoisis
+import scipy.stats as st
+
+
